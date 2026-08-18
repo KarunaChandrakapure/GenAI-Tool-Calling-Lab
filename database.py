5a695595aa371997bfd5f850ad1925694ad186e5
Karuna Chandrakapure <karunavc1234@gmail.com>
2026-08-18 20:10:57 +0530

    Handle error generated by sql

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ import mysql.connector
 from dotenv import load_dotenv
 load_dotenv()
 def execute_sql(query):
+   
     
     connection = mysql.connector.connect(
         
@@ -12,15 +13,30 @@ def execute_sql(query):
         database=os.getenv("MYSQL_DATABASE"),
         port=(os.getenv("MYSQL_PORT"))
     )
-
+      
+   
     cursor = connection.cursor()
-    cursor.execute(query)
-    results = cursor.fetchall()
+    try:
+        cursor.execute(query)
+        results = cursor.fetchall()
+        return {
+            'success':True,
+            'data':results
+        }
+    except mysql.connector.Error as e:
+        return 
+        {
+            'success':False,
+            'data':str(e)
+        }    
+
+    finally:
+        cursor.close()
+        connection.close() 
 
 
-    cursor.close()
-    connection.close()    
-    return results
+      
+  
 
 
    
